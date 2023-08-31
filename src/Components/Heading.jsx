f00b8f86afa0d8efce0bf78df342df9236517696
Kieran <kieran.webdevelopment@gmail.com>
2023-08-31 08:28:17 +0100

    Revert "button styling complete :)"
    This reverts commit 8f52c91879a1301046e31adbb61867e2979d0b1a.

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -19,39 +19,9 @@ const StyledHeading = styled.div`
     font-size: var(--headingM);
     color: var(--purple);
   }
-  button {
-    background: transparent;
+  & .play-icon {
     border: none;
-    cursor: pointer;
-    border-radius: 50%;
-  }
-  button svg circle:hover {
-    opacity: 1;
-  }
-  button svg path:hover {
-    fill: #fff;
-  }
-`;
-
-const StyledSvg = styled.svg`
-  ${
-    '' /* circle {
-    opacity: 0.25;
-  }
-
-  path {
-    fill: #a445ed;
-  } */
-  }
-
-  &:hover {
-    circle {
-      opacity: 1;
-    }
-
-    path {
-      fill: #fff;
-    }
+    background-color: transparent;
   }
 `;
 
@@ -120,19 +90,7 @@ function Heading({ allWordData }) {
         <div>
           <audio ref={audioRef} src={`${headingData.audio}`}></audio>
           <button onClick={handlePlay}>
-            <>
-              <StyledSvg
-                xmlns="http://www.w3.org/2000/svg"
-                width="75"
-                height="75"
-                viewBox="0 0 75 75"
-              >
-                <g fill="#A445ED" fillRule="evenodd">
-                  <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
-                  <path d="M29 27v21l21-10.5z" />
-                </g>
-              </StyledSvg>
-            </>
+            <img className="play-icon" src={playIcon} alt="play icon" />
           </button>
         </div>
       )}
