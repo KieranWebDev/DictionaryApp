998dc40d0cbf2dbbbd1f5740a2d75cac9036d379
Kieran <kieran.webdevelopment@gmail.com>
2023-09-01 11:23:23 +0100

    styling of definitions-container

--- src/Components/MeaningCard.jsx
+++ src/Components/MeaningCard.jsx
@@ -14,9 +14,27 @@ const StyledMeaningsCard = styled.div`
     align-items: center;
     gap: 1rem;
     margin-bottom: 2.5rem;
+    font-size: 1.5rem;
+  }
+  .part-of-speech-container h3 {
+    margin-top: -5px;
   }
   .part-of-speech-container hr {
     flex-grow: 2;
+  }
+  .definition-container span {
+    display: block;
+    margin-bottom: 1.5rem;
+  }
+  .definition-container ul {
+    margin-left: 1.5rem;
+  }
+  .definition-container li {
+    margin-bottom: 0.8rem;
+  }
+  .definition-container .example-sentence {
+    margin-top: 0.8rem;
+    color: var(--sub-text);
   }
 `;
 
@@ -40,14 +58,19 @@ function Meanings({ meaning }) {
         <h3>{meaning.partOfSpeech}</h3>
         <hr />
       </div>
-      <ul>
-        {meaning.definitions.map((item) => (
-          <li key={item.definition}>
-            <p>{item.definition}</p>
-            {item.example && <h1>{item.example}</h1>}
-          </li>
-        ))}
-      </ul>
+      <div className="definition-container">
+        <span>Meaning</span>
+        <ul>
+          {meaning.definitions.map((item) => (
+            <li key={item.definition}>
+              <p>{item.definition}</p>
+              {item.example && (
+                <p className="example-sentence">&quot;{item.example}&quot;</p>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
       <div>
         <h2>SYNONYMS</h2>
         {meaning.synonyms?.map((item) => (
