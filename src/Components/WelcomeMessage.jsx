3a9a9b8efa0af2b722df8bb40ffbbde7b4b6bed9
Kieran <kieran.webdevelopment@gmail.com>
2023-09-08 13:54:15 +0100

    styled Welcom Message

--- src/Components/WelcomeMessage.jsx
+++ src/Components/WelcomeMessage.jsx
@@ -1,9 +1,22 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.8rem;
+  text-align: center;
+  width: 80%;
+  margin: 0 auto;
+`;
+
 function WelcomeMessage() {
   return (
-    <div className="welcome-message">
+    <StyledContainer>
       <h1>Welcome to the Dictionary App</h1>
       <p>Search for a word to get started</p>
-    </div>
+    </StyledContainer>
   );
 }
 
