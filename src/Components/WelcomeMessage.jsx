90c8db2239c7935a92543b1a8d431ebd44233d8d
Kieran <kieran.webdevelopment@gmail.com>
2023-09-08 13:48:56 +0100

    created Welcome Message component

---
+++ src/Components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+  return (
+    <div className="welcome-message">
+      <h1>Welcome to the Dictionary App</h1>
+      <p>Search for a word to get started</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
